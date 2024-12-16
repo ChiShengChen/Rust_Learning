cf296e8ec1aab36767c443b196b6a1e7566393be
Michael <m50816m50816@gmail.com>
2024-12-16 20:33:12 +0800

    Add files via upload

---
+++ hello_cargo/target/debug/deps/hello_cargo-ecd8def3cace2029.d
@@ -0,0 +1,5 @@
+/home/csc/rust_proj/hello_cargo/target/debug/deps/libhello_cargo-ecd8def3cace2029.rmeta: src/main.rs
+
+/home/csc/rust_proj/hello_cargo/target/debug/deps/hello_cargo-ecd8def3cace2029.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/csc/rust_proj/hello_cargo/target/debug/hello_cargo: /home/csc/rust_proj/hello_cargo/src/main.rs
